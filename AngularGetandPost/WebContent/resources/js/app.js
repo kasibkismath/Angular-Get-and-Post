bfbcac9c37d47c563f82644f0ce45851d3f5515d
Kasib Kismath <kasibkismath@gmail.com>
2015-11-03 08:36:27 +0530

    Added the personService, service object to app.js

--- AngularGetandPost/WebContent/resources/js/app.js
+++ AngularGetandPost/WebContent/resources/js/app.js
@@ -1,16 +1,33 @@
 var personApp = angular.module('personApp', []);
 
-personApp.controller('mainController', [
-		'$scope',
-		'$http',
-		function($scope, $http) {
+personApp.service('personService', [ '$http', function($http) {
+
+	var getUrl = '/AngularGetandPost/getPerson';
+	var sendUrl = '/AngularGetandPost/sendPerson';
+	
+	// returns the person data from the database to a JSON /getPerson url
+	this.getPerson = function() {
+		return $http.get(getUrl);
+	};
+	
+	// add the person to the database add returns the update data
+	// uses the /sendPerson url
+	this.addPerson = function(personData) {
+		return $http.post(sendUrl, personData);
+	}
+
+} ]);
+
+personApp.controller('mainController', [ '$scope', 'personService',
+		function($scope, personService) {
 			
-			$http.get('/AngularGetandPost/getPerson').success(function(result) {
+			// calls the personService to get the person data
+			personService.getPerson().success(function(result) {
 				$scope.persons = result;
 			}).error(function(data, status) {
 				console.log(data);
 			});
-			
+
 			$scope.firstname = '';
 			$scope.lastname = '';
 			$scope.occupation = '';
@@ -22,17 +39,17 @@ personApp.controller('mainController', [
 					lastname : $scope.lastname,
 					occupation : $scope.occupation
 				};
+				
+				// calls the personService to add the a new person
+				personService.addPerson(personData).success(function(result) {
+					$scope.persons = result;
 
-				$http.post('/AngularGetandPost/sendPerson', personData)
-						.success(function(result) {
-							$scope.persons = result;
-
-							$scope.firstname = '';
-							$scope.lastname = '';
-							$scope.occupation = '';
-						}).error(function(data, status) {
-							console.log(data);
-						});
+					$scope.firstname = '';
+					$scope.lastname = '';
+					$scope.occupation = '';
+				}).error(function(data, status) {
+					console.log(data);
+				});
 
 			}
 		} ]);
